0950a8636e4162c7352c7d2b5c93f3c8b79b3033
emily <emilycoco@me.com>
2018-08-23 11:48:05 +1200

    routes

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const db = require('../db')
 
 const router = express.Router()
 
+// root route
 router.get('/', (req, res) => {
   db.getUsers()
     .then(users => {
@@ -12,6 +13,37 @@ router.get('/', (req, res) => {
     .catch(err => {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
+})
+
+// add a new user
+router.get('/add', (req, res) => {
+  res.render('add-user')
+})
+
+router.post('/add', (req, res) => {
+  const newUser = ({
+    name: req.body.name,
+    email: req.body.email,
+    url: req.body.url,
+    profile_picture: req.body.profile_picture
+  })
+  function redirector () {
+    res.redirect('/')
+  }
+  db.addUser(newUser, redirector)
+})
+
+// profile route
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  db.getUser(id)
+  .then(profile => {
+    console.log(profile)
+    res.render('profile', profile)
+  })
+  .catch(err => {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
 })
 
 module.exports = router
